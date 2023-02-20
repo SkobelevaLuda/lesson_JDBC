ff5692a3a7046e7292171e4f47b4394af02f4106
Людок <skobeleva.ludmila@yandex.ru>
2023-02-20 11:12:45 +0300

    Первый коммит

--- homeWork_JDBC/src/main/java/org/example/Application.java
+++ homeWork_JDBC/src/main/java/org/example/Application.java
@@ -1,2 +1,43 @@
-package org.example;public class Application {
+package org.example;
+
+import java.sql.*;
+
+public class Application {
+    public static void main(String[] args) {
+
+
+        final String user = "postgres";
+        final String password = "$Skobel2804";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+
+        // Создаем соединение с базой с помощью Connection
+        // Формируем запрос к базе с помощью PreparedStatement
+        try (final Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM employee")) {
+
+            // Делаем запрос к базе и результат кладем в ResultSet
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+
+                // С помощью методов getInt и getString получаем данные из resultSet
+                long id = resultSet.getLong("id");
+                String age = resultSet.getString("age");
+                String name = resultSet.getString(" name");
+                String surname = resultSet.getString("surname");
+                //int age = resultSet.getInt(4);
+
+                // Выводим данные в консоль
+                System.out.println(id);
+                System.out.println(name);
+                System.out.println(surname);
+                System.out.println("Возраст: " + age);
+
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
 }
+
